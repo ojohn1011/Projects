812ed02ae90ecc9103c0564bb2b26ed1fc503017
KrypticIce <KrypticIce@users.noreply.github.com>
2015-10-17 11:35:15 +0100

    Update PluginBlocker.java

--- Cove-Hub/src/me/JakeyTheDev/Hub/CommandBlocker/PluginBlocker.java
+++ Cove-Hub/src/me/JakeyTheDev/Hub/CommandBlocker/PluginBlocker.java
@@ -6,14 +6,19 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import me.JakeyTheDev.Hub.Utils.ChatUtil;
 
-public class PluginBlocker implements Listener {
+public class PluginBlocker implements Listener 
+{
 
 	@EventHandler
-	public void blockPlugin(PlayerCommandPreprocessEvent e) {
-		if(e.getMessage().contains("/pl") || e.getMessage().contains("/plugin")|| e.getMessage().contains("/?")) {
-			if(e.getPlayer().isOp()) {
+	public void blockPlugin(PlayerCommandPreprocessEvent e)
+	{
+		if(e.getMessage().contains("/pl") || e.getMessage().contains("/plugin")|| e.getMessage().contains("/?")) 
+		{
+			if(e.getPlayer().isOp())
+			{
 
-			} else {
+			} else
+			{
 				ChatUtil.sendMessage(e.getPlayer(), ChatUtil.FABULOUS, "This command could of been fabulous! But erm no.");
 				e.setCancelled(true);
 			}
